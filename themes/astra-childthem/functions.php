91faf39c1d3a114b6651eafb974b690a07cd64bc
Hael Sallam <sallamhael@gmail.com>
2024-10-28 13:01:55 +0100

    Centrer le button commander

--- themes/astra-childthem/functions.php
+++ themes/astra-childthem/functions.php
@@ -1,24 +1,40 @@
 <?php
 // Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 
-// BEGIN ENQUEUE PARENT ACTION
-// AUTO GENERATED - Do not modify or remove comment markers above or below:
-
-if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
-    function chld_thm_cfg_locale_css( $uri ){
-        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+// Ensure RTL stylesheet is loaded if needed
+if (!function_exists('chld_thm_cfg_locale_css')):
+    function chld_thm_cfg_locale_css($uri)
+    {
+        if (empty($uri) && is_rtl() && file_exists(get_template_directory() . '/rtl.css')) {
             $uri = get_template_directory_uri() . '/rtl.css';
+        }
         return $uri;
     }
 endif;
-add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
-         
-if ( !function_exists( 'child_theme_configurator_css' ) ):
-    function child_theme_configurator_css() {
-        wp_enqueue_style( 'chld_thm_cfg_child', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'astra-theme-css' ) );
+add_filter('locale_stylesheet_uri', 'chld_thm_cfg_locale_css');
+
+
+if (!function_exists('child_theme_configurator_css')):
+    function child_theme_configurator_css()
+    {
+
+        wp_enqueue_style('astra-parent-style', get_template_directory_uri() . '/style.css');
+
+
+        wp_enqueue_style('chld_thm_cfg_child', get_stylesheet_directory_uri() . '/style.css', array('astra-parent-style'), wp_get_theme()->get('Version'));
     }
 endif;
-add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
+add_action('wp_enqueue_scripts', 'child_theme_configurator_css', 20);
 
-// END ENQUEUE PARENT ACTION
+function add_admin_link_to_menu($items, $args)
+{
+    if (is_user_logged_in()) {
+
+        $admin_link = '<li><a href="' . get_admin_url() . '">Admin</a></li>';
+        $items .= $admin_link;
+    }
+    return $items;
+}
+
+add_filter('wp_nav_menu_items', 'add_admin_link_to_menu', 10, 2);
